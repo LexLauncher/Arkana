604ee2f17799a0bed46303ad55c412a85d972c4f
Jamie Mansfield <dev@jamierocks.uk>
2016-07-23 16:55:50 +0100

    Basic Macaron setup

--- arkana.go
+++ arkana.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+    "gopkg.in/macaron.v1"
+)
 
+func main() {
+    // Initialise Macaron
+    m := macaron.Classic()
+
+    // Start Web Server
+    m.Run()
 }
